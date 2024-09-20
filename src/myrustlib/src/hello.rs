37ee09db96eba90337c68928ed2766ce4ccdfbeb
Jeroen Ooms <jeroenooms@gmail.com>
2024-09-20 14:10:06 +0000

    version 1.2.1

--- src/myrustlib/src/hello.rs
+++ src/myrustlib/src/hello.rs
@@ -1,11 +1,13 @@
-use std;
 use std::ffi::CString;
 use std::os::raw::c_char;
 
 #[no_mangle]
-pub extern fn string_from_rust() -> *const c_char {
+pub extern "C" fn string_from_rust() -> *const c_char {
     let s = CString::new("Hello ピカチュウ !").unwrap();
-    let p = s.as_ptr();
-    std::mem::forget(s);
-    p
+    s.into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn free_string_from_rust(ptr: *mut c_char) {
+    let _ = unsafe { CString::from_raw(ptr) };
 }
